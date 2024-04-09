bff22dd24f9a66caf6cf72c3c27ba4616fd75246
Vanessa Radmore <146290658+IronNess@users.noreply.github.com>
2024-04-09 01:27:44 +0100

    Update maze.js

---
+++ maze.js
@@ -0,0 +1,6 @@
+const maze = document.getElementById('maze');
+const rows = 10;
+const cells = 10;
+const player = document.createElement('div');
+player.classList.add('player');
+maze.appendChild(player);
